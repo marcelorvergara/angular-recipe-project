2e132db82fdc07f09e292e446ef625e4717681d1
Marcelo Vergara <marcelorv@gmail.com>
2023-10-28 10:02:13 -0300

    Fix: bug of controls array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -16,6 +16,10 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private recipeService: RecipeService){}
 
+  get controls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls
+  }
+
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
@@ -31,18 +35,30 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = ''
     let recipeImgPath = ''
     let recipeDesc = ''
+    let recipeIngredients = new FormArray([])
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id)
       recipeName = recipe.name
       recipeImgPath = recipe.imagePath
       recipeDesc = recipe.description
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name' : new FormControl(recipeName),
       'imagePath': new FormControl(recipeImgPath),
-      'description': new FormControl(recipeDesc)
+      'description': new FormControl(recipeDesc),
+      'ingredients': recipeIngredients
     })
   }
 
