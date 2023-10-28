29ff964054dc3d765628eac075e705058da51cbd
Marcelo Vergara <marcelorv@gmail.com>
2023-10-28 10:14:04 -0300

    Form array for adding ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -65,4 +65,13 @@ export class RecipeEditComponent implements OnInit {
   onSubmit() {
     console.log('test')
   }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
 }
