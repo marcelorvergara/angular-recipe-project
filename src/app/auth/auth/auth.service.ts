535c5146d9163d96acc3a11e41d692fa637504c6
Marcelo Vergara <marcelorv@gmail.com>
2023-11-02 10:29:33 -0300

    Preparing the signup request

---
+++ src/app/auth/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToke: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=' +
+        environment.apiKey,
+      { email, password, returnSecureToken: true }
+    );
+  }
+}
