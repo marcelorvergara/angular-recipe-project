61ee1cc7b39809f4d5ef4044522fb038400ff3ef
Marcelo Vergara <marcelorv@gmail.com>
2023-11-02 09:34:13 -0300

    Fix: buf with resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -7,11 +7,16 @@ import {
 } from '@angular/router';
 import { Recipe } from './recipe.module';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 export const RecipesResolverService: ResolveFn<Recipe[]> = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
+  const recipes = inject(RecipeService).getRecipes();
+  if (recipes.length === 0) {
+    return inject(DataStorageService).fetchRecipes();
+  }
+  return recipes;
   // return this.dataStorageService.fetchRecipes();
-  return inject(DataStorageService).fetchRecipes();
 };
