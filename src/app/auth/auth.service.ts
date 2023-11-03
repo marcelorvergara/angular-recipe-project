e24889b8896360962b661b4023ab8482c2e95ed4
Marcelo Vergara <marcelorv@gmail.com>
2023-11-03 16:56:48 -0300

    Auto login feature

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -66,6 +66,28 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+    // get token method
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logOut() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -80,6 +102,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResp: HttpErrorResponse) {
